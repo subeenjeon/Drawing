9aca3d971592f41b51f51fafada38eac4629b18c
subeenjeon <powersubeen@naver.com>
2014-09-04 15:19:12 +0900

    I create js

--- sketch.js
+++ sketch.js
@@ -1,3 +1,9 @@
 function setup() {
-  ellipse(10, 10, 50, 50);
+	createCanvas(640, 480);
+	background(0);
+	stroke(255);
+	noFill();
+	line(10, 10, 90, 10);
+	rect(20, 20, 80, 50);
+	ellipse(50, 50, 50, 50);
 }
